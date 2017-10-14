ef7ef4c800b1fac06e330a91d03fe8b5a2a0d067
rk1998 <rohithk98@gmail.com>
2017-10-14 11:38:58 -0400

    added button on start screen that takes you to camera app when you click on it

--- app/src/main/java/com/calendar/cam/calendarcam/Controller/StartActivity.java
+++ app/src/main/java/com/calendar/cam/calendarcam/Controller/StartActivity.java
@@ -1,15 +1,43 @@
 package com.calendar.cam.calendarcam.Controller;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.calendar.cam.calendarcam.R;
 
 public class StartActivity extends AppCompatActivity {
-
+    private Button mTakePicButton;
+    private Button mImportPicButton;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        mTakePicButton = (Button) findViewById(R.id.take_pic_button);
+        mImportPicButton = (Button) findViewById(R.id.import_pic_button);
+
+        mTakePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Camera Intent", "Attempting to Launch Camera Intent");
+                launchCameraIntent();
+            }
+        });
+
     }
+
+    private void launchCameraIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
 }
