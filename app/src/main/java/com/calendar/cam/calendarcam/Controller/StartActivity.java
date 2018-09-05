a1e21ba9344d6867405227b12a883a8911a0aefe
rkrishnan42 <rohithk98@gmail.com>
2018-09-04 23:09:54 -0400

    integrated with Firebase to use new ml kit for text recognition

--- app/src/main/java/com/calendar/cam/calendarcam/Controller/StartActivity.java
+++ app/src/main/java/com/calendar/cam/calendarcam/Controller/StartActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.CalendarContract;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseArray;
@@ -22,13 +23,23 @@ import android.widget.Toast;
 import com.calendar.cam.calendarcam.Model.CalendarInteraction;
 import com.calendar.cam.calendarcam.Model.Model;
 import com.calendar.cam.calendarcam.R;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.android.gms.vision.Frame;
+//import com.google.android.gms.vision.text.TextBlock;
+//import com.google.android.gms.vision.text.TextRecognizer;
+
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Calendar;
+import java.util.List;
 
 public class StartActivity extends AppCompatActivity {
     private Button mTakePicButton;
@@ -106,27 +117,60 @@ public class StartActivity extends AppCompatActivity {
     }
 
     private void getTextFromImage(Bitmap image) {
-        Context context = getApplicationContext();
-        TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+        final Context context = getApplicationContext();
+        FirebaseVisionImage fireImage = FirebaseVisionImage.fromBitmap(image);
+        FirebaseVisionTextDetector detector  = FirebaseVision.getInstance().getVisionTextDetector();
+        Task<FirebaseVisionText> result = detector.detectInImage(fireImage)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        List<FirebaseVisionText.Block> textBlocks
+                                = firebaseVisionText.getBlocks();
+                        Model model = Model.get_instance();
+                        try {
+                            CalendarInteraction calendarEvent = model.process_text_boxes(textBlocks);
+                            Intent calendarIntent = new Intent(Intent.ACTION_INSERT)
+                                    .setData(CalendarContract.Events.CONTENT_URI)
+                                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                                            calendarEvent.getStartTime().getTimeInMillis() )
+                                    .putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                                            calendarEvent.getEndTime().getTimeInMillis())
+                                    .putExtra(CalendarContract.Events.TITLE,
+                                            calendarEvent.getEventName());
+                            startActivity(calendarIntent);
 
-        Frame frame = new Frame.Builder().setBitmap(image).build();
-        SparseArray<TextBlock> items = textRecognizer.detect(frame);
+                        } catch(IllegalArgumentException iae) {
+                            Toast.makeText(context, iae.getMessage(), Toast.LENGTH_LONG).show();
 
-        Model model = Model.get_instance();
+                        }
 
-        try {
-            CalendarInteraction calendarEvent = model.process_text_boxes(items);
-
-            Intent calendarIntent = new Intent(Intent.ACTION_INSERT)
-                    .setData(CalendarContract.Events.CONTENT_URI)
-                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calendarEvent.getStartTime().getTimeInMillis() )
-                    .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calendarEvent.getEndTime().getTimeInMillis())
-                    .putExtra(CalendarContract.Events.TITLE, calendarEvent.getEventName());
-            startActivity(calendarIntent);
-        } catch (IllegalArgumentException iae) {
-            System.out.println(iae.getMessage());
-            Toast.makeText(this, iae.getMessage(), Toast.LENGTH_LONG).show();
-        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(context, "No Text Detected", Toast.LENGTH_LONG).show();
+                    }
+                });
+//        TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+//        Frame frame = new Frame.Builder().setBitmap(image).build();
+//        SparseArray<TextBlock> items = textRecognizer.detect(frame);
+//
+//        Model model = Model.get_instance();
+//
+//        try {
+//            CalendarInteraction calendarEvent = model.process_text_boxes(items);
+//
+//            Intent calendarIntent = new Intent(Intent.ACTION_INSERT)
+//                    .setData(CalendarContract.Events.CONTENT_URI)
+//                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calendarEvent.getStartTime().getTimeInMillis() )
+//                    .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calendarEvent.getEndTime().getTimeInMillis())
+//                    .putExtra(CalendarContract.Events.TITLE, calendarEvent.getEventName());
+//            startActivity(calendarIntent);
+//        } catch (IllegalArgumentException iae) {
+//            System.out.println(iae.getMessage());
+//            Toast.makeText(this, iae.getMessage(), Toast.LENGTH_LONG).show();
+//        }
 
     }
 
--- app/src/main/java/com/calendar/cam/calendarcam/Model/Model.java
+++ app/src/main/java/com/calendar/cam/calendarcam/Model/Model.java
@@ -4,9 +4,9 @@ import android.util.Log;
 import android.util.SparseArray;
 
 import com.google.android.gms.vision.text.TextBlock;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 
@@ -51,7 +51,6 @@ public class Model {
     }
     /**Singleton Instance**/
     private static final Model _instance = new Model();
-    private static final int BASE = 599;
     /**
      * Gets singleton instance of model
      * @return The singleton instance of model
@@ -70,13 +69,10 @@ public class Model {
             throw new IllegalArgumentException("No Text Detected");
         }
         String eventName = "";
-        String date = "";
-        String time = "";
         int month = 0;
         int day = 0;
         String startTime = "7PM";
         String endTime = "7PM";
-        List<TextBlock> camTextList = new LinkedList<>();
         int maxArea = Integer.MIN_VALUE;
         int index = 0;
         for(int i = 0; i < camTextArray.size(); i++) {
@@ -194,7 +190,162 @@ public class Model {
             }
 
         }
-        CalendarInteraction calendar = new CalendarInteraction(eventName, 2017, month, day, startTime, endTime);
+        CalendarInteraction calendar = new CalendarInteraction(eventName,
+                2017, month, day, startTime, endTime);
+/*        Log.d("Day", "" + day);
+        Log.d("Month", ""+ month);
+        Log.d("startTime", startTime);
+        Log.d("Month after", "" + calendar.getStartMonth());
+        Log.d("Year", "" + ""+calendar);*/
+        return calendar;
+    }
+
+    /**
+     * Processes Text blocks found by OCR API, this method uses Firebase ML Vision API
+     * @param blocks blocks of text found by API
+     * @return Calendar Interaction event with parsed Date/Time info
+     */
+    public CalendarInteraction process_text_boxes(List<FirebaseVisionText.Block> blocks) {
+        if(blocks.isEmpty()) {
+            throw new IllegalArgumentException("No Text Detected");
+        }
+        String eventName = "";
+        int month = 0;
+        int day = 0;
+        String startTime = "7PM";
+        String endTime = "7PM";
+        int maxArea = Integer.MIN_VALUE;
+        int index = 0;
+        for(int i = 0; i < blocks.size(); i++) {
+            FirebaseVisionText.Block textBlock = blocks.get(i);
+            int bboxArea = textBlock.getBoundingBox().height() * textBlock.getBoundingBox().width();
+            if(bboxArea > maxArea) {
+                maxArea = bboxArea;
+                index = i;
+            }
+        }
+        eventName = blocks.get(index).getText();
+        for(int i = 0; i < blocks.size(); i++) {
+            FirebaseVisionText.Block text = blocks.get(i);
+            String textValue = text.getText();
+            Log.d("TEXT: ", textValue);
+            Month[] months = Month.values();
+            String[] textArray = textValue.toLowerCase().split("\\s+|-");
+
+            // Find Time
+            //TODO: there is a problem when words contain "am" or "pm" in them, need to check
+            //if these strings have any numbers in them
+            int firstAMIdx = stringContainsSearch("am", textArray, 0);
+            int firstPMIdx = stringContainsSearch("pm", textArray, 0);
+
+            Log.d("FirstAMIdx:", ""+firstAMIdx);
+            Log.d("FirstPMIdx:", ""+firstPMIdx);
+            if (firstAMIdx != -1 || firstPMIdx != -1) {
+                //Check if array entry contains number, if not, earlier does
+                if (firstAMIdx != -1) {
+                    //Looking at AM
+                    if (Character.isDigit(textArray[firstAMIdx].charAt(0))) {
+                        startTime = textArray[firstAMIdx];
+                    } else {
+                        startTime = textArray[firstAMIdx - 1] + textArray[firstAMIdx];
+                    }
+                } else {
+                    //Looking at PM
+                    if(textArray.length == 1) {
+                        if(Character.isDigit(textArray[firstPMIdx].charAt(0))) {
+                            startTime = textArray[firstPMIdx];
+                        }
+                    } else {
+                        if (Character.isDigit(textArray[firstPMIdx].charAt(0))) {
+                            startTime = textArray[firstPMIdx];
+                        } else {
+                            startTime = textArray[firstPMIdx - 1] + textArray[firstPMIdx];
+                        }
+                    }
+
+                }
+            }
+            int secondAMIdx = stringContainsSearch("am", textArray, firstAMIdx + 1);
+            int secondPMIdx = stringContainsSearch("pm", textArray, firstPMIdx + 1);
+            if (secondAMIdx != -1 || secondPMIdx != -1) {
+                //Check if array entry contains number, if not, earlier does
+                if (secondAMIdx != -1) {
+                    //Looking at AM
+                    if (Character.isDigit(textArray[secondAMIdx].charAt(0))) {
+                        endTime = textArray[secondAMIdx];
+                    } else {
+                        endTime = textArray[secondAMIdx - 1] + textArray[secondAMIdx];
+                    }
+                } else {
+                    //Looking at PM
+                    if (Character.isDigit(textArray[secondPMIdx].charAt(0))) {
+                        endTime = textArray[secondPMIdx];
+                    } else {
+                        endTime = textArray[secondPMIdx - 1] + textArray[secondPMIdx];
+                    }
+
+                }
+            }
+
+
+            for(Month m: months) {
+
+                int zeroresult = -1;
+                int zeroAbrvResult = -1;
+                //Special case,
+                if (m.get_longForm().equals("october")) {
+                    zeroresult = stringSearch("0ctober", textArray);
+                    zeroAbrvResult = stringSearch("0ct", textArray);
+                }
+                int result = stringSearch(m.get_longForm(), textArray);
+                int abbrvResult = stringSearch(m.get_abbreviation(), textArray);
+                if(result != -1 || zeroresult != -1) {
+                    month = m.get_monthNum();
+                    //Todo: Need to handle case when day field has "th" i.e. "October 9th"
+                    if (zeroresult != -1) {
+                        if (Character.isDigit(textArray[zeroresult - 1].charAt(0))) {
+                            day = Integer.parseInt(textArray[zeroresult - 1]);
+                        } else {
+                            day = Integer.parseInt(textArray[zeroresult + 1]);
+                        }
+                        break;
+                    } else {
+                        if (result == 0) {
+                            day = Integer.parseInt(textArray[result + 1]);
+                        } else if (Character.isDigit(textArray[result -1].charAt(0))) {
+                            day = Integer.parseInt(textArray[result - 1]);
+                        } else {
+                            day = Integer.parseInt(textArray[result + 1]);
+                        }
+                        break;
+                    }
+
+
+                } else if(abbrvResult != -1 || zeroAbrvResult != -1) {
+                    month = m.get_monthNum();
+                    if (zeroAbrvResult != -1) {
+                        if (Character.isDigit(textArray[zeroAbrvResult - 1].charAt(0))) {
+                            day = Integer.parseInt(textArray[zeroAbrvResult - 1]);
+                        } else {
+                            day = Integer.parseInt(textArray[zeroAbrvResult + 1]);
+                        }
+                        break;
+                    } else {
+                        if(abbrvResult == 0) {
+                            day = Integer.parseInt(textArray[abbrvResult + 1]);
+                        } else if (Character.isDigit(textArray[abbrvResult - 1].charAt(0))) {
+                            day = Integer.parseInt(textArray[abbrvResult - 1]);
+                        } else {
+                            day = Integer.parseInt(textArray[abbrvResult + 1]);
+                        }
+                        break;
+                    }
+                }
+            }
+
+        }
+        CalendarInteraction calendar = new CalendarInteraction(eventName,
+                2017, month, day, startTime, endTime);
 /*        Log.d("Day", "" + day);
         Log.d("Month", ""+ month);
         Log.d("startTime", startTime);
